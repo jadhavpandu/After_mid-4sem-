61a817ed9012eb726a0666f05898ef98dbf0e991
jadhavpandu <2022bit054@sggs.ac.in>
2024-03-15 10:47:26 +0530

    comparing the strings

---
+++ after mid/string1.java
@@ -0,0 +1,8 @@
+class string1{
+    public static void main(String[] args){
+        String a = "hello";
+        String b = new String("hello");
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+    }
+}
