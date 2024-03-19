fdb8c1a2de7cae43e57b0cb4b0cb08b11ec124f6
jadhavpandu <2022bit054@sggs.ac.in>
2024-03-19 07:44:34 +0530

    ==  operator and equals()

--- after mid/Practice.java
+++ after mid/Practice.java
@@ -1,22 +1,20 @@
-class demo{
-  public int add(int a , int b){
-    int result =a+b;
-    return result;
-  }
-  public int add(int a , int b,int c){
-    int result =a+b+c;
-    return result;
-  }
-
-}
- 
 
 public class Practice {
     public static void main(String[] args){
-       demo obj = new demo();
-      int res1 = obj.add(2,3 );
-      int res2= obj.add(1,2,3);
-        System.out.println(res1);
-        System.out.println(res2);
+      String a= "virat";
+      System.out.println(a);
+      String b = "virat";
+      System.out.println(b);
+      //which the reference of object are equall or not
+      System.out.println(a==b);
+      //which ceheck the content of object are equal or not
+      System.out.println(a.equals(b));
+    String s1 = new String("virat");
+    System.out.println(s1);
+      String s2 = new String("virat");
+      System.out.println(s2);
+      System.out.println(s1==s2);
+      System.out.println(s1.equals(s2));
+      
     }
 }
