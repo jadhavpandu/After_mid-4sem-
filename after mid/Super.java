ac982fab48bb64f574458714308653b5dbe228cd
jadhavpandu <2022bit054@sggs.ac.in>
2024-03-20 11:02:37 +0530

    deep learning of super()

--- after mid/Super.java
+++ after mid/Super.java
@@ -1,20 +1,29 @@
+class demo{
+  int x, y;
+  demo(){
+    System.out.println("zero parameters constructor");
+  }
+  demo(int x , int y){
+    System.out.println("x");
+    System.out.println("y");
+  }
+}
+class demo1 extends demo{
+  int a ,b;
+  demo1(){
+    super();
+     System.out.println("this is child class of demo class");
+  }
+  demo1(int a , int b ){
+    this();
+     System.out.println("a");
+     System.out.println("b");
+  }
+  }
 
-  class demo{
-    public demo(){
-        System.out.println("this is parent class");
-    }
-    public demo( int x, int y){
-        System.out.println("this will execute by super()");
-    }
+public class Super{
+  public static void main(String[] args) {
+    //demo1 sub = new demo1();
+    demo1 sub1 = new demo1(2,5);
   }
-  class demo1 extends demo{
-    int x,y;
-    public void  dallo(){
-        System.out.println("this s child class");
-    }
-  }
-public class Super {
-    public static void main(String[] args) {
-       demo1 a = new demo1(); 
-    }
 }
